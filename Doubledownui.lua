20d3a9d0e95d8542507a1fd501a15e9d0b9933b9
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-12 18:56:50 +0300

    Update Doubledownui.lua

--- Doubledownui.lua
+++ Doubledownui.lua
@@ -1,1 +1,38 @@
-niggers have big black cock
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
+
+
+
+local FirstWindow = OrionLib:MakeWindow({Name = "SyrHub x Double Down UI", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = SyrOnTop})
+
+
+-- Tabs
+local ToolTab = Window:MakeTab({
+	Name = "Tools",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local MainTab = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local GeneralTab = Window:MakeTab({
+	Name = "General Use",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+-- Sections
+
+local ScriptsSection = Tab:AddSection({
+	Name = "Scripts"
+})
+
+ToolTab:AddButton({
+	Name = "Fullbright",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/06iG6YkU", true))()
+  	end    
+})
