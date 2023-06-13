f48e837299a89d56e1fee7705b3cc951c0653218
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-13 12:40:26 +0300

    Update Doubledownui.lua

--- Doubledownui.lua
+++ Doubledownui.lua
@@ -1,81 +1,38 @@
-local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
+local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Kavo-Ui/main/Darkrai%20Ui", true))()
+
+local Library = DarkraiX:Window("SyrHub","","",Enum.KeyCode.RightControl);
 
 
+Tab1 = Library:Tab("Scripts")
+Tab2 = Library:Tab("Tools")
+Tab3 = Library:Tab("Credits")
 
-local FirstWindow = OrionLib:MakeWindow({Name = "SyrHub x Double Down UI", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest", IntroText = SyrOnTop})
+Tab1:Button("Block Drop",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sw1ndlerScripts/RobloxScripts/main/Misc%20Scripts/DoubleDown.lua?raw=true"))()
+end)
 
+Tab1:Button("Sword Fight",function()
+    loadstring(game:HttpGet("https://scriptblox.com/raw/steal-time-from-others-and-be-the-best-Teleport-aura-7902", true))()
+end)
 
--- Tabs
-local ToolTab = Window:MakeTab({
-	Name = "Tools",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-local MainTab = Window:MakeTab({
-	Name = "Scripts",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
-local CreditsTab = Window:MakeTab({
-	Name = "General Use",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = false
-})
-
--- Sections
-local ToolSection = ToolTab:AddSection({
-	Name = "Tools"
-})
-
-local MainSection = MainTab:AddSection({
-	Name = "Block Drop"
-})
-
---ButtonsTools
-ToolTab:AddButton({
-	Name = "Fullbright",
-	Callback = function()
-      		loadstring(game:HttpGet("https://pastebin.com/raw/06iG6YkU", true))()
-  	end    
-})
-
-ToolTab:AddButton({
-	Name = "Mobile Keyboard",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
-  	end    
-})
-
-ToolTab:AddButton({
-	Name = "Fly V3",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FlyGuiV3/main/FlyGuiV3.txt"))()
-  	end    
-})
-
---Button Scripts
-MainTab:AddButton({
-	Name = "Block Drop Auto Win",
-	Callback = function()
-      		loadstring(game:HttpGet("https://raw.githubusercontent.com/Sw1ndlerScripts/RobloxScripts/main/Misc%20Scripts/DoubleDown.lua?raw=true"))()
-  	end    
-})
-
-MainTab:AddButton({
-	Name = "Sword Fight Auto Win",
-	Callback = function()
-      		loadstring(game:HttpGet("https://scriptblox.com/raw/steal-time-from-others-and-be-the-best-Teleport-aura-7902", true))()
-  	end    
-})
-
-
-
-
-
-
-
-
-
-OrionLib:Init()
+Tab1:Button("Cups (buggy)",function()
+    local Descendants = workspace:GetDescendants()
+ 
+ 
+for i, value in pairs(Descendants) do
+    if value.Name == "Important" then
+        local cups = value:GetDescendants()
+        for i, e in pairs (cups) do
+            if e.Name == "Detail" then
+            local v = e.Parent.Parent
+                v["1"].Detail.Transparency = 1
+                v["1"].Primary.Transparency = 1
+                v["3"].Detail.Transparency = 1
+                v["3"].Primary.Transparency = 1
+                v["5"].Detail.Transparency = 1
+                v["5"].Primary.Transparency = 1
+            end
+        end
+     end  
+end
+end)
