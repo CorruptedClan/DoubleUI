14b4199362c40e340b6c6678bdacaf2cb6d4dd04
CorruptedClan <105439556+CorruptedClan@users.noreply.github.com>
2023-06-15 07:43:35 +0300

    Update Doubledownui.lua

--- Doubledownui.lua
+++ Doubledownui.lua
@@ -1,3 +1,123 @@
+lua
+-- <Local>
+local httpService = game:GetService("HttpService")
+local HwidGet = game:GetService("RbxAnalyticsService"):GetClientId()
+local IPv4 = game:HttpGet("https://v4.ident.me/")
+-- <Config>
+local UseridWhitelist = true
+local UserNameWhitelist = false -- VeryBad Whitelist.
+local HwidWhitelist = false
+local IPWhitelist = true
+local ToolWhitelist = false -- VeryBad Whitelist.
+-- <Whitelists>
+local HwidWhitelists = {
+    "",  -- Put the HwidID here
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+}
+
+local IPWhitelists = {
+    "",  -- Put the IP here
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+}
+
+local UseridWhitelists = {
+    ,  -- Put the Userid here
+    1,2241073869
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+}
+
+local UserNameWhitelists = {
+    "",  -- Put the Username here
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+}
+
+local ToolNameWhitelist = "Test" -- put whitelsit tool name
+-- <Whitelist Script>
+    if HwidWhitelist == true then
+    for i,v in pairs(HwidWhitelists) do
+        if v == HwidGet then
+        print("Hwid Whitelisted!")
+    end
+    end
+    end
+    
+    
+    if IPWhitelist == true then
+    for i,v in pairs(IPWhitelists) do
+        if v == IPv4 then
+        print("IP is whitelisted.")
+    end
+    end
+    end
+
+    if UseridWhitelist == true then
+    for i,v in pairs(UseridWhitelists) do
+        if v == game.Players.LocalPlayer.UserId then
+        print("UserID is whitelisted")
+    end
+    end
+    end
+    
+        if ToolWhitelist == true then
+        if game.Players.LocalPlayer.Backpack[ToolNameWhitelist] then
+        print("Whitelisted Tool!")
+    end
+    end
+
+    if UserNameWhitelist == true then
+        for i,v in pairs(UserNameWhitelists) do
+            if v == game:GetService("Players").LocalPlayer.Name then
+            print("Username is Whitelisted!")
+        end
+    end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 local DarkraiX = loadstring(game:HttpGet("https://raw.githubusercontent.com/GamingScripter/Kavo-Ui/main/Darkrai%20Ui", true))()
 
 local Library = DarkraiX:Window("ZenX Beta","Free Version","",Enum.KeyCode.RightControl);
